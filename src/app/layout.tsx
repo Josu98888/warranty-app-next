f71ff72cddc7fb3d2ed43f9c6adf4b9d33f046f2
Josu <aquinojosue889@gmail.com>
2026-07-24 22:42:06 -0300

    feat: add page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Sidebar from "@/shared/components/layouts/Sidebar";
+import Topbar from "@/shared/components/layouts/Topbar";
 
 export const metadata: Metadata = {
   title: "Warranty App",
@@ -13,7 +15,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <main className="min-h-screen bg-[#F6F8FC]">
+          <div className="flex flex-col lg:flex-row">
+            <Sidebar />
+            <section className="flex-1 flex flex-col w-full pt-16 lg:pt-0">
+              <Topbar />
+              <div className="p-4 sm:p-6 lg:p-10">{children}</div>
+            </section>
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
